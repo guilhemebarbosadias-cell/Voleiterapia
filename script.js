2b47768a1bc86c1496eaef9e678a5c2a2490f973
guilhemebarbosadias-cell <guilhemebarbosadias@gmail.com>
2026-08-06 10:26:24 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ let numeroSelecionado="";
 let itemAtual={};
 
 // URL do Google Apps Script integrada
-const URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbx8f4qCMipm3WqyVCrlZXho1BcKu732xIESzkZTufgpQJtw-WIuJ4_cTfoKf_8GXpiV/exec"
+const URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbwoTbeb_jXc1UcgYPFvjVIQmmZ3_yi4sK7Nd2Obyj4S6eXsnRCZeyNKZ02s9S9V66Px/exec"
 const valores={
 camisa:75,
 calcaoMasc:35,
